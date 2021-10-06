d8eb17570c72a873479ed74cbbb231952fc26b8c
mys1erious <mys1erious@users.noreply.github.com>
2021-10-06 10:49:32 +0300

    upd

---
+++ Year 3/Term 1/dotnet/labs/Lab1/Lab1/2021_Lavrinovych_E_O_Lab_1.cs
@@ -0,0 +1,30 @@
+﻿namespace Lab1
+// http://csharphelper.com/blog/2015/06/make-a-fraction-class-in-c/ Example
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
+class Fraction
+{
+    private int numerator;
+    private int denominator;
+    public Fraction(string fraction)
+    {
+        string[] splitFraction = fraction.Split('/');
+        numerator = int.Parse(splitFraction[0]);
+        denominator = int.Parse(splitFraction[1]);
+        
+    }
+
+    public Fraction(int numerator_, int denominator_)
+    {
+        numerator = nunumerator_;
+        denominator = denominator_;
+    }
+}
