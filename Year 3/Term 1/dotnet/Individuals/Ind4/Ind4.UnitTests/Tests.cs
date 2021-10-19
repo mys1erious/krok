dfd1d2c9e6452e5a77ad71eb5069a02bda6216c7
mys1erious <mys1erious@users.noreply.github.com>
2021-10-19 21:57:15 +0300

    upd

---
+++ Year 3/Term 1/dotnet/Individuals/Ind4/Ind4.UnitTests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Ind4.UnitTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
